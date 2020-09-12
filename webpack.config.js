df173b314130f104cabe18bba628ec789b5de2f3
texas2010 <9701609+texas2010@users.noreply.github.com>
2020-09-11 23:14:44 -0500

    install webpack and delete some files

---
+++ webpack.config.js
@@ -0,0 +1,55 @@
+const myPublicPath = 'docs'
+
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  entry: ['@babel/polyfill', './src/js/index.js'],
+  output: {
+    path: path.resolve(__dirname, myPublicPath),
+    filename: 'js/bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: { loader: "babel-loader" }
+      },
+      {
+        test: /\.html$/,
+        use: [
+          { loader: "html-loader" }
+        ]
+      },
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader']
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "src", "index.html")
+    }),
+    new MiniCssExtractPlugin({
+      filename: '[name].css',
+    })
+  ],
+  devServer: {
+    contentBase: './' + myPublicPath
+  },
+  optimization: {
+    splitChunks: {
+      cacheGroups: {
+        styles: {
+          name: 'styles',
+          test: /\.css$/,
+          chunks: 'all',
+          enforce: true,
+        },
+      },
+    },
+  }
+}
